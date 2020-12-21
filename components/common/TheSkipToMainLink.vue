711c42fefe132ee1f7fa3de8c1410da57582db65
d4g0 <tocarralero@gmail.com>
2020-12-21 15:41:59 -0500

    mjm

--- components/common/TheSkipToMainLink.vue
+++ components/common/TheSkipToMainLink.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="">
-    <nuxt-link :to="localePath('/#main')"
-    class="absolute left-2 top-2 z-70  text-center block p-3 border border-gray-300 bg-white rounded-kush shadow-2xl transition-all duration-150  focus:outline-none focus:ring-2 -mt-32 focus:mt-0"
+    <nuxt-link
+      @keydown="handleKey"
+      :to="localePath('/#main')"
+      class="absolute left-2 top-2 z-70 text-center block p-3 border border-gray-300 bg-white rounded-kush shadow-2xl transition-all duration-150 focus:outline-none focus:ring-2 -mt-32 focus:mt-0"
       >Skip to main content</nuxt-link
     >
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    function handleKey(evt) {
+      console.log(evt.key);
+    }
+    return {
+      handleKey,
+    };
+  },
+};
 </script>
 
 <style scoped>
-.up-8{
-    transform: translate3d(0,-8rem,0);
+.up-8 {
+  transform: translate3d(0, -8rem, 0);
 }
-.focus\:mt-0:focus{
-margin-top: 0;
+.focus\:mt-0:focus {
+  margin-top: 0;
 }
 </style>
